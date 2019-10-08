8d8a9e873770a194ff1fcb9a9a3df69e7cae2070
xphant0mx <55114524+xphant0mx@users.noreply.github.com>
2019-10-07 21:59:46 -0400

    Add files via upload

--- Team4_Assgn2_KNN_Ex_2.5.py
+++ Team4_Assgn2_KNN_Ex_2.5.py
@@ -145,9 +145,9 @@ K = 15
 knn_diabetes = KNeighborsRegressor(n_neighbors=K)
 #Predict target values based on test data
 knn_diabetes.fit(X_train, Y_train)
-n15_predict = knn_diabetes.predict(X_test)
-rmse_scores = np.sqrt(mean_squared_error(Y_test, n15_predict))
-display_scores(rmse_scores)
+n15_scores = cross_val_score(knn_diabetes,  X_test, Y_test, scoring="neg_mean_squared_error")
+n15_rmse = np.sqrt(-n15_scores)
+display_scores(n15_rmse)
 
 
 # In[10]:
@@ -191,9 +191,9 @@ plt.title('KNN Regressor, K=18')
 # Average RMSE for KNN Regressor with K =18 for Test Data
 #Predict target values based on test data
 knn_diabetes.fit(X_train, Y_train)
-n18_predict = knn_diabetes.predict(X_test)
-rmse_scores = np.sqrt(mean_squared_error(Y_test, n18_predict))
-display_scores(rmse_scores)
+n18_scores = cross_val_score(knn_diabetes,  X_test, Y_test, scoring="neg_mean_squared_error")
+n18_rmse = np.sqrt(-n18_scores)
+display_scores(n18_rmse)
 
 
 # In[ ]:
